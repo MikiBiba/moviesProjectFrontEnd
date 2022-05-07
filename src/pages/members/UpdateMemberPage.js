d2581b659a6897cbe610053cc386e905becbca88
Miki Biba <mikibiba4@gmail.com>
2022-05-07 17:24:05 +0300

    Updating edit member page

--- src/pages/members/UpdateMemberPage.js
+++ src/pages/members/UpdateMemberPage.js
@@ -1,11 +1,69 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import { useParams } from "react-router";
+import { getOneItem, updateItem } from "../../utils";
 const EditMemberPage = () => {
+
+  const { id } = useParams();
+
+  const [member, setMember] = useState({
+    name: "",
+    email: "",
+    city: "",
+  });
+
+  const membersUrl = "http://localhost:8000/members";
+
+  const handleUpdate = async () => {
+    const result = await updateItem(membersUrl, member._id, member);
+    alert(result)
+  };
+
+  useEffect(() => {
+    const fetchData = async (url) => {
+      const { data } = await getOneItem(url, id);
+      setMember(data);
+    };
+    fetchData(membersUrl);
+  }, []);
+
+
+
   return (
     <div>
-      <h2>Edit Members page!</h2>
+      <h2>{`Edit ${member.name}'s data`}</h2>
+      Name:{" "}
+      <input
+        type="text"
+        value={member.name}
+        onChange={(e) => setMember({ ...member, name: e.target.value })}
+      />
+      <br />
+      Email:{" "}
+      <input
+        type="text"
+        value={member.email}
+        onChange={(e) =>
+          setMember({ ...member, email: e.target.value })
+        }
+      />
+      <br />
+      City:{" "}
+      <input
+        type="text"
+        value={member.city}
+        onChange={(e) => setMember({ ...member, city: e.target.value })}
+      />
+      <br />
+      <br />
+      <button onClick={handleUpdate}>Update</button>
+
+      <Link to={"/members"}>
+        <button>Cancel</button>
+      </Link>
     </div>
   );
 };
+
 
 export default EditMemberPage;
--- src/pages/movies/UpdateMovie.js
+++ src/pages/movies/UpdateMovie.js
@@ -30,13 +30,7 @@ const UpdateMovie = () => {
     fetchData(moviesUrl);
   }, []);
 
-  //   const handleSubmit = async (e) => {
-  //     e.preventDefault();
-  //     if (movie) {
-  //       const { result } = await addNewItem(url, movie);
-  //       alert("Movie added succefully", result);
-  //     }
-  //   };
+
 
   return (
     <div>
@@ -72,40 +66,7 @@ const UpdateMovie = () => {
       />
       <br />
       <button onClick={handleUpdate}>Update</button>
-      {/* <form onSubmit={handleSubmit}>
-        Name:{" "}
-        <input
-          type="text"
-          onChange={(e) => setMovie({ ...movie, name: e.target.value })}
-        />{" "}
-        <br />
-        Genres:{" "}
-        <input
-          type="text"
-          onChange={(e) =>
-            setMovie({ ...movie, genres: e.target.value.split(",") })
-          }
-        />{" "}
-        <br />
-        Image URL:{" "}
-        <input
-          type="text"
-          onChange={(e) => setMovie({ ...movie, imageUrl: e.target.value })}
-        />{" "}
-        <br />
-        Premiered:{" "}
-        <input
-          type="text"
-          onChange={(e) =>
-            setMovie({ ...movie, yearPremiered: e.target.value })
-          }
-        />{" "}
-        <br /> <br />
-        <button type="submit">Send</button>{" "}
-        <Link to="/movies">
-          <button type="button">Cancel</button>
-        </Link>
-      </form> */}
+
       <Link to={"/movies"}>
         <button>Cancel</button>
       </Link>
